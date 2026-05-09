f4a69648cc13886cd31b9e9c34c7ab2021a32fa4
Sarfaraz Ahmad <ahmadsarfaraz8657@gmail.com>
2026-05-09 06:45:59 +0000

    Initial Elix setup

---
+++ app/src/main/java/com/elix/ai/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.elix.ai
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        title = "Elix AI"
+    }
+}
