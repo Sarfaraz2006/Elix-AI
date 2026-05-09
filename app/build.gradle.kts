76afa9944016a5208874136dd2ad6a8dcff232b5
Sarfaraz Ahmad <ahmadsarfaraz8657@gmail.com>
2026-05-09 09:18:22 +0000

    Fix Room Gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
@@ -62,5 +63,9 @@ dependencies {
 }
 
 plugins {
-    kotlin("kapt")
+
+dependencies {
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 }
